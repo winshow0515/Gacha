a0d4d0db89b918604cbcd4203edbc144c5cf4326
winshow <winshow0515@gmail.com>
2025-08-19 12:33:54 +0800

    Read run vlc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
 
 reed_button = 17  # GPIO17 = Pin 11
@@ -8,25 +9,37 @@ delaySec = 0.1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(reed_button, GPIO.IN)
 
+vedio_path = "./example.mp4"
 
 def destroy():
     GPIO.cleanup()
 
 
-def showResult():
+def startReed():
     prev_input = 1
     while True:
         input_result = GPIO.input(reed_button)
         if input_result == 0 and prev_input == 1:
             print("Button Pressed")
-            time.sleep(2) #避免重複觸發
+            play_animation()
+            time.sleep(11) #避免重複觸發
         prev_input = input_result
         print(input_result)
         time.sleep(delaySec)
 
+def play_animation():
+    subprocess.Popen([
+        "cvlc",
+        "--play-and-exit",
+        "--fullscreen",
+        "--no-osd",
+        vedio_path
+    ])
+
+
 
 if __name__ == "__main__":
     try:
-        showResult()
+        startReed()
     except KeyboardInterrupt:
         destroy()
